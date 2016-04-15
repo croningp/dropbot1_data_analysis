25823352bca7bded7ce019465e4f04cf4bb9940d
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-04-15 11:31:27 +0100

    ga finalized

--- optimization/ga/ga.py
+++ optimization/ga/ga.py
@@ -26,6 +26,33 @@ def proba_normalize(x):
     return list(x / numpy.sum(x))
 
 
+def genome_crossover(genome1, genome2):
+    neonate = [None] * len(genome1)
+
+    for i in range(len(neonate)):
+        rnd = random.random()
+        # 25% of chance to inherit from 1
+        if 0 <= rnd < 0.25:
+            neonate[i] = genome1[i]
+        # 25% of chance to inherit from 2
+        elif 0.25 <= rnd < 0.5:
+            neonate[i] = genome2[i]
+        # 50% of chance to inherit a mix of both
+        else:
+            # the mix is of random proportion of 1 and 2
+            rnd_mix = random.random()
+            neonate[i] = rnd_mix * genome1[i] + (1 - rnd_mix) * genome2[i]
+    return neonate
+
+
+def genome_mutation(genome, per_locus_rate, per_locus_SD):
+    neonate = list(genome)
+    for i in range(len(neonate)):
+        if random.random() < per_locus_rate:
+            neonate[i] += random.gauss(0, per_locus_SD)
+    return neonate
+
+
 class GA(object):
     """
     A strategy that will keep track of the basic parameters of the GA
@@ -35,6 +62,8 @@ class GA(object):
     :param pmin: Min value a genome can take for each dimension
     :param pmax: Max value a genome can take for each dimension
     :param n_survivors: Number of survivors to next generation
+    :param per_locus_rate: Probability of mutation on each gene (default 0.3)
+    :param per_locus_SD: Standard deviation of Gaussian used for the mutation amplitude (default 0.1)
     :param temp: Temperature for the softmax at survival selection step (default 1)
     """
 
@@ -45,7 +74,8 @@ class GA(object):
         self.pmin = pmin
         self.pmax = pmax
         self.n_survivors = n_survivors
-
+        self.per_locus_rate = kargs.get('per_locus_rate', 0.3)
+        self.per_locus_SD = kargs.get('per_locus_SD', 0.1)
         self.temp = float(kargs.get('temp', 1))
 
         self.setup()
@@ -75,6 +105,10 @@ class GA(object):
         genome = creator.Individual(random.uniform(self.pmin, self.pmax) for _ in range(self.part_dim))
         return genome
 
+    def generate_empty_genome(self):
+        genome = [None for _ in range(self.part_dim)]
+        return genome
+
     def get_next_population(self):
         return self.population
 
@@ -98,10 +132,13 @@ class GA(object):
     def update_population(self):
         survivors_id = self.draw_survivors_id()
 
-        new_population = [self.generate_genome() for _ in range(self.pop_size)]
+        new_population = [self.generate_empty_genome() for _ in range(self.pop_size)]
 
-        for i in survivors_id:
-            new_population[i] = self.population[i]
+        for i in range(len(new_population)):
+            if i in survivors_id:
+                new_population[i] = self.population[i]
+            else:
+                new_population[i] = self.breed_one_child()
 
         self.population = new_population
 
@@ -119,6 +156,26 @@ class GA(object):
 
         return survivors_id
 
+    def breed_one_child(self):
+        fitnesses = self.get_fitnesses()
+        x = soft_max(fitnesses, self.temp)
+
+        parents = []
+        for i in range(2):
+            x = proba_normalize(x)
+            ind = probabilistic_choice(x)
+            parents.append(self.population[ind])
+            x[ind] = 0
+
+        neonate = genome_crossover(parents[0], parents[1])
+        neonate = genome_mutation(neonate, self.per_locus_rate, self.per_locus_SD)
+        neonate = self.clip_genome(neonate)
+        return creator.Individual(neonate)
+
+    def clip_genome(self, genome):
+        for i in range(len(genome)):
+            genome[i] = float(numpy.clip(genome[i], self.pmin, self.pmax))
+        return genome
 
 if __name__ == "__main__":
 
@@ -133,7 +190,9 @@ if __name__ == "__main__":
               'part_dim': 3,
               'pmin': -1,
               'pmax': 1,
-              'n_survivors': 5,
+              'n_survivors': 2,
+              'per_locus_rate': 0.3,
+              'per_locus_SD': 0.1,
               'temp': 0.1}
 
     optimizer = GA(**params)
